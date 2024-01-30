f409279f55182820525634586cae8b350d4e4e31
Tatiana <tanya18082000@mail.ru>
2024-01-30 13:57:41 +0300

    test: origins

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
+    "https://localhost:3000",
+    "https://localhost:5000"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
